0ade278e96ec3933753aa788b81545c7413bf5f0
Danibomfin <98532797+Danibomfin@users.noreply.github.com>
2022-08-06 07:55:42 -0300

    Add files via upload

---
+++ assets/preload/data/crossed-out/PreLoad.lua
@@ -0,0 +1,30 @@
+function onCreate()
+    --BG and Effects--
+    --Boyfriend--
+    addCharacterToList('bf_bendy_1', 'boyfriend')
+    addCharacterToList('bf_bendy_2', 'boyfriend')
+    addCharacterToList('bf_bendy_3', 'boyfriend')
+    addCharacterToList('bf_cuphead_1', 'boyfriend')
+    addCharacterToList('bf_cuphead_2', 'boyfriend')
+    addCharacterToList('bf_final', 'boyfriend')
+    addCharacterToList('bf_sans_1', 'boyfriend')
+    addCharacterToList('bf_sans_2', 'boyfriend')
+    addCharacterToList('bf_sans_3', 'boyfriend')
+    addCharacterToList('bf_sans_4', 'boyfriend')
+    --Characters--
+    addCharacterToList('bendy_normal', 'dad')
+    addCharacterToList('bendy_run', 'dad')
+    addCharacterToList('bendy_run_dark', 'dad')
+    addCharacterToList('cuphead_angry', 'dad')
+    addCharacterToList('final_bendy', 'dad')
+    addCharacterToList('final_cuphead', 'dad')
+    addCharacterToList('final_sans', 'dad')
+    addCharacterToList('sans_attack', 'dad')
+    addCharacterToList('sans_normal', 'dad')
+    addCharacterToList('sans_angry', 'dad')
+    addCharacterToList('sans_snow', 'dad')
+    addCharacterToList('cuphead_angry', 'gf')
+    --Bg and Effects High Quality--
+  
+    --Amogusyta--
+end
